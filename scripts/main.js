3e50b5db46d9fe822f3914c5ba3e21511d8d88e4
andrejaques <andrejaques.b@gmail.com>
2021-01-06 04:26:22 -0300

    random function
    added the random number function

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,8 @@ function searchFunction()
     let imput = document.getElementById('search').value;
     window.location.href=`https://google.com/search?q=${imput}`;
 }
+
+function randomFunction() 
+{
+    window.location.href=`https://google.com/search?q=random`;
+}
