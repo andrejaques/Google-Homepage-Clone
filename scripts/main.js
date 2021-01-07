226993cd77960348a11b7b68de0db4434c4c8210
andrejaques <andrejaques.b@gmail.com>
2021-01-06 23:49:22 -0300

    news

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 // SCRIPTS
+textbox = document.getElementById('search');
 
 function searchFunction() 
 {
@@ -10,3 +11,14 @@ function randomFunction()
 {
     window.location.href=`https://google.com/search?q=random`;
 }
+
+textBox.addEventListener("keyup", function (e) // is not complete..
+{ 
+  
+    // Checking if key pressed is ENTER or not 
+    // if the key pressed is ENTER 
+    // click listener on button is called 
+    if (e.keyCode || e.which == 13) { 
+        searchFunction(); 
+    } 
+});
